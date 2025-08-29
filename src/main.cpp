9359528ade8b4c0b8c03669879c7537f4ae034b0
Shazil04 <malikshazilms04@gmail.com>
2025-08-29 14:36:44 +0500

    new

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-#include<fstream>
-#include<vector>
 using namespace std;
 
 int main() {
     // TODO: Implement expense tracker here
     cout << "Expense Tracker Project Initialized!" << endl;
+    cout << "Let's get started!";
     return 0;
 }

8946730160946b007eb584ba18b7a21e8ab7fa85
Shazil04 <malikshazilms04@gmail.com>
2025-08-29 15:45:02 +0500

    expense class

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
 using namespace std;
+class Expense {
+    private:int ID ;
+    private:string date;
+    private:string category;
+    private:double amount;
+    private:string note;
+
+    Expense(int ID , const string& date , const string &category , double amount , const string & note){
+        this->ID = ID;
+        this->date = date;
+        this->category = category;
+        this->amount = amount;
+        this->note = note;
+    }
+    public:void setAmount(double myAmount) {
+        if (myAmount < 0) {
+            cout << "Invalid Amount " << myAmount << endl;
+        }
+        else {
+            amount = myAmount;
+        }
+    }
+    public:string getCategory() {
+        return category;
+    }
+    public:double getAmount() const {
+        return amount;
+    }
+    public:void getDate(string &myDate) const {
+        myDate = date;
+    }
+};
+
+
 
 int main() {
-    // TODO: Implement expense tracker here
+
     cout << "Expense Tracker Project Initialized!" << endl;
     return 0;
 }

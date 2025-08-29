0aa8a56e3d865801a03cc890bcca5ed6ab2fed5d
Shazil04 <malikshazilms04@gmail.com>
2025-08-29 14:45:31 +0500

    removed

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,5 @@ using namespace std;
 int main() {
     // TODO: Implement expense tracker here
     cout << "Expense Tracker Project Initialized!" << endl;
-    cout << "Let's get started!";
-    cout << "Let's get started!";
     return 0;
 }
